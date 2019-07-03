809eefb85a4743a04f36386136b7bfc3b5f76b24
Takeshi Kishi <takeya0x86@gmail.com>
2019-07-04 01:01:12 +0900

    Firefox full page screenshot test

--- src/test/java/com/example/selenium/FirefoxTest.java
+++ src/test/java/com/example/selenium/FirefoxTest.java
@@ -1,13 +1,19 @@
 package com.example.selenium;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -15,7 +21,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 class FirefoxTest {
 
-  private WebDriver driver;
+  private FirefoxDriver driver;
 
   @BeforeAll
   static void beforeAll() {
@@ -33,6 +39,24 @@ class FirefoxTest {
       driver.quit();
     }
   }
+
+  @Test
+  void testFullPageScreenshot() throws IOException {
+    driver.get("https://github.com/");
+    Path screenShot = driver.getScreenshotAs(OutputType.FILE).toPath();
+    Files.copy(screenShot, Paths.get("sc01.png"), REPLACE_EXISTING);
+
+    screenShot = driver.getFullPageScreenshotAs(OutputType.FILE).toPath();
+    Files.copy(screenShot, Paths.get("sc02.png"), REPLACE_EXISTING);
+
+    driver.findElement(By.linkText("open source")).click();
+    screenShot = driver.getScreenshotAs(OutputType.FILE).toPath();
+    Files.copy(screenShot, Paths.get("sc03.png"), REPLACE_EXISTING);
+
+    screenShot = driver.getFullPageScreenshotAs(OutputType.FILE).toPath();
+    Files.copy(screenShot, Paths.get("sc04.png"), REPLACE_EXISTING);
+  }
+
 
   @Test
   void testGet() {
